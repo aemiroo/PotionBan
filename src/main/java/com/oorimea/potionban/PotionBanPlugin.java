eb55bcb36ce1a03d1f93137e14c09f0f894eb109
aemiroo <86331285+aemiroo@users.noreply.github.com>
2024-05-13 01:08:18 +0800

    Update PotionBanPlugin.java
    Added Collectors and Filter

--- src/main/java/com/oorimea/potionban/PotionBanPlugin.java
+++ src/main/java/com/oorimea/potionban/PotionBanPlugin.java
@@ -1,41 +1,32 @@
 package com.oorimea.potionban;
 
-
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffectType;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class PotionBanPlugin extends JavaPlugin {
 
-    private Set<PotionEffectType> bannedEffects;
+    private Set<PotionEffectType> bannedEffects = new HashSet<>();
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         reloadConfig();
-
         getServer().getPluginManager().registerEvents(new PotionListener(this), this);
 
-        getLogger().info("PotionBan enabled.");
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("PotionBan disabled.");
+        getLogger().info("Banned Effects: " + String.join(", ", getBannedEffects().stream().map(PotionEffectType::getName).toList()));
     }
 
     public void reloadConfig() {
         super.reloadConfig();
-        bannedEffects = new HashSet<>();
-        for (String effectName : getConfig().getStringList("banned-effects")) {
-            try {
-                bannedEffects.add(PotionEffectType.getByName(effectName));
-            } catch (IllegalArgumentException e) {
-                getLogger().warning("Invalid potion effect: " + effectName);
-            }
-        }
+        bannedEffects = getConfig().getStringList("banned-effects").stream()
+                .map(PotionEffectType::getByName)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
     }
 
     public Set<PotionEffectType> getBannedEffects() {
